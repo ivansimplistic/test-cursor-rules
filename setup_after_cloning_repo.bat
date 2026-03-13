eda9f209be03050676ae9e473f016f4df9caa76c
Ivan Simplistic <ivan@simplistic.com>
2026-03-13 10:23:07 -0300

    add commands to setup

--- setup_after_cloning_repo.bat
+++ setup_after_cloning_repo.bat
@@ -1,2 +1,9 @@
 git config core.hooksPath .githooks
+
+git remote add -f uno-cursor-rules https://github.com/Simplistic-GE/uno-cursor-rules
+git merge -s ours --no-commit --allow-unrelated-histories uno-cursor-rules/main
+git read-tree --prefix= -u uno-cursor-rules/main
+git commit -m "Subtree merged in uno-cursor-rules"
+git subtree pull --prefix=.cursor/rules/ uno-cursor-rules main
+
 pause
---
+++ setup_update_cursor_rules.bat
@@ -0,0 +1,3 @@
+git subtree pull --prefix=.cursor/rules/ uno-cursor-rules main
+
+pause
